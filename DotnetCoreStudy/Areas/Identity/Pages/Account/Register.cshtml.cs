fda30e64895b2bfb38b0884671c28fb1a7bb5939
wisuwat <wisuwatpond01@gmail.com>
2024-09-26 16:02:37 +0700

    Create Admin & Employee Account

--- DotnetCoreStudy/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DotnetCoreStudy/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -207,7 +207,14 @@ namespace DotnetCoreStudy.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Create Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
