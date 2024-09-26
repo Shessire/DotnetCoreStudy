2ead7b8d46a8a49fcde821ab2fee1d664a78a935
wisuwat <wisuwatpond01@gmail.com>
2024-09-26 16:57:27 +0700

    Implement DbInitializer

--- DotnetCoreStudy.DataAccess/DbInitializer/DbInitializer.cs
+++ DotnetCoreStudy.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Azure.Identity;
+using DotnetCoreStudy.DataAccess.Data;
+using DotnetCoreStudy.Models;
+using DotnetCoreStudy.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +14,57 @@ namespace DotnetCoreStudy.DataAccess.DbInitializer
 {
     public class DbInitializer : IDbInitializer
     {
-        public void Initialize() { 
-            throw new NotImplementedException();
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager, 
+            RoleManager<IdentityRole> roleManager, 
+            ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public void Initialize() {
+
+            //migrations if they are not applied
+            try { 
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex) { }
+
+            //create roles if they are not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+                //if roles are not created, then we will create admin user as well
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@dotnetstudy.com",
+                    Email = "admin@dotnetstudy.com",
+                    Name = "ABC",
+                    PhoneNumber = "1112223333",
+                    StreetAddress = "test 123 Ave",
+                    State = "IL",
+                    PostalCode = "23422",
+                    City = "Chicago"
+                }, "Test123;").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@dotnetstudy.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+            return;
+            
         }
     }
 }
